d1083ce674e130f8e768cf2a126b19f538e74fba
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-18 19:52:28 -0300

    feat: last touches on about me

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -7,7 +7,7 @@ import Components.Svg as SVG exposing (Logo(..))
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route
 import Html exposing (Attribute, Html, a, br, button, div, footer, h1, h2, h3, h5, header, img, li, nav, p, section, span, text, ul)
-import Html.Attributes exposing (attribute, class, href, id, rel, src, tabindex, target)
+import Html.Attributes exposing (alt, attribute, class, href, id, rel, src, tabindex, target)
 import Html.Attributes.Aria exposing (ariaLabel, ariaLabelledby)
 import Html.Events exposing (on, onCheck, onClick)
 import Layout exposing (initLayout, rootId)
@@ -177,7 +177,7 @@ viewHeader model =
           else
             text ""
         , links
-            ++ [ a [ href "#", class "list__resume" ] [ text "resume" ] ]
+            ++ [ li [] [ a [ href "#", class "list__resume" ] [ text "resume" ] ] ]
             |> ul [ class "list" ]
         ]
     ]
@@ -214,6 +214,15 @@ viewPage model =
 
 viewMainContent : Model -> Html Msg
 viewMainContent model =
+    div [ class "main grid gap-10 w-[min(100vw_-_2rem,1920px)] lg:w-full mx-auto z-10" ]
+        [ viewSectionOne model
+        , viewSectionTwo model
+        , viewSectionThree model
+        ]
+
+
+viewSectionOne : Model -> Html Msg
+viewSectionOne model =
     let
         textSize width str =
             if model.viewport.w >= width then
@@ -222,65 +231,90 @@ viewMainContent model =
             else
                 ""
     in
-    div [ class "main w-[min(100vw_-_2rem,1920px)] lg:w-full mx-auto z-10" ]
-        [ section
-            [ class "grid place-content-center gap-5 min-h-screen m-auto select-none py-24"
-            , ariaLabelledby "title--name"
+    section
+        [ class "grid place-content-center gap-5 min-h-screen m-auto select-none py-24"
+        , ariaLabelledby "title--name"
+        ]
+        [ Html.i [ class "font-mono text-accent-600 text-sm" ]
+            [ text "hi, my name is" ]
+        , h1 [ class "text-7xl font-800", id "title--name" ]
+            [ [ "Johann", textSize 1920 "Gonçalves", "Pereira" ]
+                |> String.join " "
+                |> text
             ]
-            [ Html.i [ class "font-mono text-accent-600 text-sm" ]
-                [ text "hi, my name is" ]
-            , h1 [ class "text-7xl font-800", id "title--name" ]
-                [ [ "Johann", textSize 1920 "Gonçalves", "Pereira" ]
-                    |> String.join " "
-                    |> text
-                ]
-            , h2 [ class "text-7xl font-800" ]
-                [ [ "I", textSize 1920 "love to", "build", textSize 1440 "things", "for the web." ]
-                    |> String.join " "
-                    |> text
-                ]
-            , p [ class "inline-block text-surface-400 w-gold-paragraph" ]
-                [ text """I’m a software developer specializing in
+        , h2 [ class "text-7xl font-800" ]
+            [ [ "I", textSize 1920 "love to", "build", textSize 1440 "things", "for the web." ]
+                |> String.join " "
+                |> text
+            ]
+        , p [ class "inline-block text-surface-400 sm:w-gold-paragraph" ]
+            [ text """I’m a software developer specializing in
              building (and occasionally designing) exceptional digital experiences. Currently,
               I'm focused on building the plataform for """
-                , a [ class "text-accent-600", href "https://app.materialize.pro" ] [ text "Materialize" ]
-                , text "."
-                ]
-            , a [ class "btm-accent mt-8", href "https://github.com/Johann-Goncalves-Pereira" ]
-                [ text "Check my GitHub" ]
+            , a [ class "text-accent-600", href "https://app.materialize.pro" ] [ text "Materialize" ]
+            , text "."
             ]
-        , section [ class "about-me" ]
-            [ header [ class "header" ]
-                [ Html.i [ class "header__number" ] [ text "01." ]
-                , h3 [ class "header__title" ] [ text "About Me" ]
-                ]
-            , p [ class "paragraph" ]
-                [ text """So perhaps, you've generated some fancy text, 
+        , a [ class "btm-accent mt-8", href "https://github.com/Johann-Goncalves-Pereira" ]
+            [ text "Check my GitHub" ]
+        ]
+
+
+viewSectionTwo : Model -> Html Msg
+viewSectionTwo model =
+    section [ class "about-me" ]
+        [ header [ class "header" ]
+            [ Html.i [ class "header__number" ] [ text "01." ]
+            , h3 [ class "header__title" ] [ text "About Me" ]
+            ]
+        , p [ class "paragraph" ]
+            [ text """So perhaps, you've generated some fancy text, 
                 and you're content that you can now copy and paste your fancy 
                 text in the comments section of funny cat videos, but perhaps 
                 you're wondering how it's even possible to change the font of 
                 your text? Is it some sort of hack? Are you copying and pasting 
                 an actual font?"""
-                , br [] []
-                , br [] []
-                , text """Well, the answer is actually no - rather than generating fancy fonts, 
+            , br [] []
+            , br [] []
+            , text """Well, the answer is actually no - rather than generating fancy fonts, 
                 this converter creates fancy symbols. The explanation starts with unicode; 
                 an industry standard which creates the specification for thousands of different 
                 symbols and characters. All the characters that you see on your 
                 electronic devices, and printed in books, are likely specified by the 
                 unicode standard."""
-                ]
-            , footer [ class "footer" ]
-                [ ul [ class "footer__list" ] <|
-                    List.map
-                        (\x ->
-                            li [ class "footer__item" ]
-                                [ materialIcon "footer__icon" "arrow_right"
-                                , text x
-                                ]
-                        )
-                        [ "elm", "css/sass/scss", "html", "docker", "shell (bash, zsh)", "go (golang)" ]
-                ]
-            , div [ class "img" ] [ img [ src "https://picsum.photos/1200" ] [] ]
+            , br [] []
+            , br [] []
+            , text """So perhaps, you've generated some fancy text, 
+                and you're content that you can now copy and paste your fancy 
+                text in the comments section of funny cat videos, but perhaps 
+                you're wondering how it's even possible to change the font of 
+                your text? Is it some sort of hack? Are you copying and pasting 
+                an actual font? Content that you can now copy and paste your fancy 
+                text in the comments section of funny cat videos, but perhaps 
+                you're wondering how it's even possible to change the font of 
+                your text? Is it some sort of hack? Are you copying and pasting 
+                an actual font?"""
             ]
+        , footer [ class "footer" ]
+            [ ul [ class "footer__list" ] <|
+                List.map
+                    (\x ->
+                        li [ class "footer__item" ]
+                            [ materialIcon "footer__icon" "arrow_right"
+                            , text x
+                            ]
+                    )
+                    [ "elm"
+                    , "html - css/sass"
+                    , "design - figma"
+                    , "Js - Ts - React"
+                    , "shell (bash, zsh)"
+                    , "go (golang)"
+                    ]
+            ]
+        , div [ class "img" ] [ img [ src "https://picsum.photos/1200", alt "Profile Photo" ] [] ]
         ]
+
+
+viewSectionThree : Model -> Html Msg
+viewSectionThree model =
+    section [] []
