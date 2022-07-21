d41f43e5ae7867d2bed511f36f3001276f573472
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-07-21 15:36:13 -0300

    feat: remake

--- src/Utils/Func.elm
+++ src/Utils/Func.elm
@@ -1,5 +1,13 @@
-module Utils.Func exposing (aplR, regex, regexValidate, run)
+module Utils.Func exposing
+    ( Patterns(..)
+    , aplR
+    , getPattern
+    , regex
+    , regexValidate
+    , run
+    )
 
+import Dict exposing (Dict)
 import Regex
 import Task
 
@@ -7,6 +15,11 @@ import Task
 aplR : a -> (a -> b) -> b
 aplR f x =
     x f
+
+
+run : msg -> Cmd msg
+run m =
+    Task.perform (always m) (Task.succeed ())
 
 
 regex : String -> Regex.Regex
@@ -20,6 +33,34 @@ regexValidate pattern_ =
     Regex.contains <| regex pattern_
 
 
-run : msg -> Cmd msg
-run m =
-    Task.perform (always m) (Task.succeed ())
+
+-- patterns : Dict String String -> String
+
+
+type Patterns
+    = Email
+
+
+patterns : Patterns -> String
+patterns pattern =
+    case pattern of
+        Email ->
+            "email"
+
+
+dictPatterns : Dict String String
+dictPatterns =
+    List.map2
+        (\n p ->
+            ( n, p )
+        )
+        [ patterns Email ]
+        [ "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,8}$" ]
+        |> Dict.fromList
+
+
+getPattern : Patterns -> String
+getPattern p =
+    dictPatterns
+        |> Dict.get (patterns p)
+        |> Maybe.withDefault ""
