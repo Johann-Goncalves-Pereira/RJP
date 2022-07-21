2c2ac5a3325815661a4d97557755539f00b529c9
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-07-21 20:01:49 -0300

    feat: in-view

--- src/Utils/Scroll.elm
+++ src/Utils/Scroll.elm
@@ -1,29 +1,79 @@
 port module Utils.Scroll exposing (..)
 
+import InView
+
 
 port onScroll : ({ x : Float, y : Float } -> msg) -> Sub msg
 
 
 type alias Model =
-    { x : Float, y : Float }
+    { inView : InView.State
+    , offset : { x : Float, y : Float }
+    }
 
 
-init : Model
+init : ( Model, Cmd Msg )
 init =
-    { x = 0, y = 0 }
+    let
+        ( inViewModel, inViewCmd ) =
+            InView.init InViewMsg listIds
+    in
+    ( { inView = inViewModel
+      , offset = { x = 0, y = 0 }
+      }
+    , inViewCmd
+    )
 
 
 type Msg
     = OnScroll { x : Float, y : Float }
+    | InViewMsg InView.Msg
+    | OnLoadElement String
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         OnScroll offset ->
-            { model | x = offset.x, y = offset.y }
+            ( { model
+                | inView = InView.updateViewportOffset offset model.inView
+                , offset = { x = offset.x, y = offset.y }
+              }
+            , Cmd.none
+            )
+
+        InViewMsg inViewMsg ->
+            let
+                ( inView_, cmd_ ) =
+                    InView.update InViewMsg inViewMsg model.inView
+            in
+            ( { model | inView = inView_ }
+            , cmd_
+            )
+
+        OnLoadElement id_ ->
+            let
+                ( inView, cmd_ ) =
+                    InView.addElements InViewMsg [ id_ ] model.inView
+            in
+            ( { model | inView = inView }
+            , cmd_
+            )
 
 
-subScroll : Sub Msg
-subScroll =
-    onScroll OnScroll
+subs : Model -> Sub Msg
+subs model =
+    Sub.batch
+        [ InView.subscriptions InViewMsg model.inView
+        , onScroll OnScroll
+        ]
+
+
+listIds : List String
+listIds =
+    [ "about"
+    , "experience"
+    , "work"
+    , "other-noteworthy-projects"
+    , "contact"
+    ]
